9f44f0ca724074524c937898067b5006b013e33d
dushyant0007 <dushyantjakhar007@gmail.com>
2024-01-02 00:45:52 +0530

    m-10-completed

--- a-js/j-m10-async-await-promises-AJAX-error-handling/e-async-await.js
+++ a-js/j-m10-async-await-promises-AJAX-error-handling/e-async-await.js
@@ -19,20 +19,30 @@ function whereAmI1() {
 // when this function is done then it automatically returns a promise
 // we cant use await without async fun - at-least for now
 async function whereAmI(country) {
-    //here we can have multiple await statements
-    //await will stop the code execution at this point of the function
-    //until the promise is fulfilled
-    const res = await fetch(`https://restcountries.com/v2/name/${country}`)
-    const data = await res.json();
-    //return 10; // return a promise with value 10 // (resolved value fo the promise)
-    return data;
+    try {
+        //here we can have multiple await statements
+        //await will stop the code execution at this point of the function
+        //until the promise is fulfilled
+        const res = await fetch(`https://restcountries.com/v2/name/${country}`)
+        if (!res.ok) throw new Error('problem-getting result')
+        const data = await res.json();
+        //return 10; // return a promise with value 10 // (resolved value fo the promise)
+        return data;
+    }
+    catch (err) {
+        console.log("error-block", err);
+        //rejected promise returned from the async fun
+        throw err;
+
+        //if i have written return err; -it will go to .than((here)=>{})
+    }
 }
 
 //^ the async and await are simply syntactic sugar over the then method in promises
 
 //a is a promise
 //value inside the promise won't be accessible synchronously without using .then() or await.
-let a = whereAmI('india')
+let a = whereAmI('inoi')
 
 //at this point of the code the js has simply no way of knowing yet
 //there's a data-var that we want ;
@@ -43,21 +53,28 @@ let a = whereAmI('india')
 console.log(a) //  return a pending promise (make sense)
 
 //----------------------------------
-a.then((val)=>console.log(val))
-.catch((err)=>{console.log(err)})
-.finally(()=>{console.log('everything is done')})
+
+//if any error is occurred in try block then
+// execution will never reach to return statement in whereAmI fun
+//even if there was an error in async fun, the promise that the async fun returns is still fulfilled
+// and not rejected 
+// if we want to catch that error in .catch(below) then we have to rethrow that error from catch block (above)
+
+a.then((val) => console.log('--11--',val, '--11--'))
+    .catch((err) => { console.log('--22--',err, '--22--') })
+    .finally(() => { console.log('everything is done') });
 
 //--or
 
-(async function(){
-    try{
+(async function () {
+    try {
         const data = await whereAmI('usa');
-        console.log(city)
+        console.log(data)
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
-    console.log('finally everything is done')
+    console.log('finally everything is done');
 })();
 
 // -----------------------------
