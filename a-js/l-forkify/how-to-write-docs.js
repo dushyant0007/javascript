78fca8d763366d567b56f6d96567ad65d36af035
dushyant0007 <dushyantjakhar007@gmail.com>
2024-01-06 02:31:54 +0530

    docs

---
+++ a-js/l-forkify/how-to-write-docs.js
@@ -0,0 +1,15 @@
+//refer to jsdoc.app to learn how to write documentation
+
+/**
+ * Render the received object to the dom
+ * @param {Object | Object[]} data The to be rendered 
+ * @param {boolean} [render=true] this is optional so we warped with []
+ * @returns {undefined | string} a markup  string is returned if render=false
+ * @this {object} View instance
+ * @author Mr Bla-bla-bal
+ * @todo what is remaining to do or the implementation is finished 
+ */
+
+function one (data,render = true){
+
+}
